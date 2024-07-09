ec53638b72ace6d535d6b0377df09a558fa8146c
Vitor Paz <mkg.vitorpaz@gmail.com>
2024-07-09 14:53:10 -0300

    Update matrizPar.js

--- matrizPar.js
+++ matrizPar.js
@@ -1,28 +1,36 @@
-function criaMatriz(linhas, colunas) {
-    let matriz = [];
-    let contador = 0;
+<script>
 
-    for (let i = 0; i < linhas; i++) {
+    //função para fazer e guardar matriz na memória
+    var criaMatriz = function(linhas, colunas){
+    var matriz = [];
+    var contador = 0;
+
+    for (var i = 0; i < linhas; i++) {
         matriz[i] = [];
-        for (let j = 0; j < colunas; j++) {
+        for (var j = 0; j < colunas; j++) {
             matriz[i][j] = contador;
             contador += 2;
         }
     }
-
     return matriz;
-}
-
-function mostrar(matriz) {
-    for (let i = 0; i < matriz.length; i++) {
-        let linha = "";
-        for (let j = 0; j < matriz[i].length; j++) {
-            linha += matriz[i][j] + "\t";
-        }
-        console.log(linha.trim());
     }
-}
 
-let minhaMatriz = criaMatriz(10, 10);
-console.log("Matriz gerada:");
-mostrar(minhaMatriz);
+    //funcão para mostrar a matriz
+    var mostrar = function(matriz) {
+        for (var i = 0; i < matriz.length; i++) {
+            var linha = "";
+            for (var j = 0; j < matriz[i].length; j++) {
+                linha += matriz[i][j] + "\t";
+            }
+            console.log(linha.trim());
+        }
+    }
+
+    //variavel para guardar a matriz
+    var minhaMatriz = criaMatriz(10, 10);
+
+    //mostrando a matriz no prompt
+    console.log("Matriz gerada:");
+    mostrar(minhaMatriz);
+
+</script>

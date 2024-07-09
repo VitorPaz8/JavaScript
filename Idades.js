f63a90d0eff59c44039bfe23191562b82a589812
Vitor Paz <mkg.vitorpaz@gmail.com>
2024-07-09 14:34:41 -0300

    Update Idades.js

--- Idades.js
+++ Idades.js
@@ -1,33 +1,33 @@
-var pulaLinha = function(){
-    document.write("<br><hr>");
-}
+<script>
+    function mostrarTela(frase){
+        document.write(frase + "<br>");
+    }
 
-var mostra = function(frase){
-    document.write("<h1>"+ frase +"</h1");
-    pulaLinha();
-}
-
-var duvida = prompt("Gostaria de calcular idades hoje? (Sim/Não)");
-
-duvida = duvida.toLowerCase();
-
-if (duvida === "sim" || duvida === "ss") {
-    var idade1 = prompt("Digite a sua idade");
-    var idade2 = prompt("Digite a idade da segunda pessoa");
-    var idade3 = prompt("Digite a idade da terceira pessoa");
-
-    idade1 = parseInt(idade1);
-    idade2 = parseInt(idade2);
-    idade3 = parseInt(idade3);
-
-    mostra("Sua idade é: " + idade1);
-
-    var somaIdades = idade1 + idade2 + idade3;
+    function mostrarPrompt(frase){
+        console.write(frase + "<br>");
+    }
     
-    mostra("A soma das idades é: " + somaIdades);
-    mostra("A média das idades é: " + (somaIdades / 3));
-} else {
-    alert("fim do programa")
-    mostra("Desculpe, estamos programados apenas para calcular idades.");
-    mostra("Fique à vontade para voltar quando quiser, quem sabe até lá já não atualizamos essa funcionalidade.");
-}
+    //pedindo para o usuario uma entrada e tornando-a minuscula
+    var duvida = prompt("Gostaria de calcular idades hoje? (Sim/Não)");
+    duvida = duvida.toLowerCase();
+    
+    if (duvida === "sim" || duvida === "ss") {
+        var idade1 = prompt("Digite a sua idade");
+        var idade2 = prompt("Digite a idade da segunda pessoa");
+        var idade3 = prompt("Digite a idade da terceira pessoa");
+    
+        idade1 = parseInt(idade1);
+        idade2 = parseInt(idade2);
+        idade3 = parseInt(idade3);
+    
+        mostraTela("Sua idade é: " + idade1);
+    
+        var somaIdades = idade1 + idade2 + idade3;
+        
+        mostraTela("A soma das idades é: " + somaIdades);
+        mostraTela("A média das idades é: " + (somaIdades / 3));
+        
+    } else {
+        alert("fim do programa")
+        }
+</script>

431310fda9266186e93ce99f6907f848ba7ec118
Vitor Paz <mkg.vitorpaz@gmail.com>
2024-07-09 14:38:05 -0300

    Update Idades.js

--- Idades.js
+++ Idades.js
@@ -2,10 +2,6 @@
     function mostrarTela(frase){
         document.write(frase + "<br>");
     }
-
-    function mostrarPrompt(frase){
-        console.write(frase + "<br>");
-    }
     
     //pedindo para o usuario uma entrada e tornando-a minuscula
     var duvida = prompt("Gostaria de calcular idades hoje? (Sim/Não)");
@@ -20,12 +16,12 @@
         idade2 = parseInt(idade2);
         idade3 = parseInt(idade3);
     
-        mostraTela("Sua idade é: " + idade1);
+        mostrarTela("Sua idade é: " + idade1);
     
         var somaIdades = idade1 + idade2 + idade3;
         
-        mostraTela("A soma das idades é: " + somaIdades);
-        mostraTela("A média das idades é: " + (somaIdades / 3));
+        mostrarTela("A soma das idades é: " + somaIdades);
+        mostrarTela("A média das idades é: " + (somaIdades / 3));
         
     } else {
         alert("fim do programa")

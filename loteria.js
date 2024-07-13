d9d0ee407ff1500f91e2455c4fd37ae276b0ec9c
Vitor Paz <mkg.vitorpaz@gmail.com>
2024-07-13 14:34:38 -0300

    atualização da loteria

--- loteria.js
+++ loteria.js
@@ -1,18 +1,74 @@
-var segredos = [16, 34, 37, 42, 50, 58];
-var caixaDoNumero = document.getElementById("numero");
+var numeroUsuario = [];
+var numeroSortiado = [];
 
-function botaoClicado() {
-    for(var i = 0; i<3; i++){
-        j = i + 1;
+//sorteando numeros
+function sorteaNumero(){
 
-        if(segredos[i] == caixaDoNumero.value) {
-            alert("Parabéns! Você acertou o " + j + "° número secreto");
-            return;
+    numeroSortiado = [];
+
+    for(var i = 0; i < 6; i++){
+        var numero = (Math.floor(Math.random() * 60) + 1);
+        numeroSortiado.push(numero)
+        
+        // ver se pussui numeros sorteados iguais
+        for(var j = 0; j < i; j++){
+            if(numeroSortiado[i] == numeroSortiado[j]){
+                numeroSortiado[i] = 0;
+                i--;
+            }
         }
-        }
-        alert("Infelizmente você errou!");
-        caixaDoNumero.value = "";
+    }
+    console.log(numeroSortiado);
 };
 
-var botaoAdivinhar = document.getElementById("adivinhar");
-botaoAdivinhar.onclick = botaoClicado;
+function adicionarNumero() {
+    
+    numeroUsuario = []
+    
+    for(var i = 0; i < 6; i++){
+
+        var numero = 0, vezes = i + 1;
+
+        numero = parseInt(prompt("Insira o " + vezes + "° numero!"));
+
+        // Checando se o numero pode ser adicionado
+        for (var j = 0; j < 6; j++) {
+            if (isNaN(numero) || numero > 60 || numero < 1) {
+                alert("Por favor, insira um número inteiro de 1 a 60.");
+                i--;
+                break;
+            } else if(numero == numeroUsuario[j]) {
+                alert("Número já escolhido!");
+                i--;
+                break;
+            }
+            numeroUsuario[j] = numero;
+        }
+    }
+};
+
+
+function comparaNumeros(){
+    var acertos = 0;
+
+    for(var i = 0; i < 6; i++){
+        for(var j = 0; j < 6; j++){ 
+            if(numeroSortiado[i] == numeroUsuario[j]){
+                acertos++;
+            }
+        }
+    }
+    if(acertos == 0){
+        alert("Você não acertou nenhum numero!");
+    }else{
+        alert("Parabêns! Você acertou " + acertos + " vezes!");
+    }
+};
+
+var botao = document.getElementById("verificar");
+botao.onclick = adicionarNumero;
+
+sorteaNumero();
+
+var botao3 = document.getElementById("comparar")
+botao3.onclick = comparaNumeros;
